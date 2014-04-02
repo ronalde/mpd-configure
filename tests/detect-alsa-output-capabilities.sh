c8446f42d8c39ce92c12db7d57076ed5aec5bb2e
Ronald van Engelen <ronalde@lacocina.nl>
2014-04-02 11:29:51 +0200

    rewritten to fix a problem watching the stream

--- tests/detect-alsa-output-capabilities.sh
+++ tests/detect-alsa-output-capabilities.sh
@@ -6,135 +6,198 @@
 ## source:    https://github.com/ronalde/mpd-configure
 ## also see:  http://lacocina.nl/2013/04/19/alsa-highres/
 
-paconf=~/.pulse/client.conf
-paconfbackup="${paconf}.da-backup"
-paconfrestore="false"
-paconfcreated="false"
-defaultcard=""
-alsaoutputdevices=0
-procasound="/proc/asound"
+MPD_HOST="localhost"
+MPD_PORT="6600"
+
+CMD_TELNET="$(which telnet)"
+PA_CLIENTCONF_FILE=~/.pulse/client.conf
+PA_CLIENTCONF_PRESENT="$([[ -f ${PA_CLIENTCONF_FILE} ]])"
+PA_CLIENT_CONF_BACKUP="${PA_CLIENT_CONF}.da-backup"
+PA_CONF_RESTORE="false"
+PA_CONF_CREATED="false"
+
+ALSA_OUTPUT_DEVICES=0
+PROC_ASOUND="/proc/asound"
+PROC_USB_STREAM=""
+
+echo -e "conf present: ${PA_CLIENTCONF_PRESENT}"
+## array to store all alsa hw:x,y addresses
+declare -a ALSA_HW_ADDRESSES=()
+SELECTED_HW_ADDRESS=""
 
 LANG=C
 
-checkparunning() {
-    # check if pulseaudio is running for the current user
-    parunning="$(pgrep -u ${USER} pulseaudio)"
+
+echo_stderr() {
+    echo -e "$@" 1>&2; 
+}
+
+die() {
+    echo_stderr "\nError: $@"
+    exit 1
+}
+
+inform() {
+    echo_stderr "$@"
+}
+
+debug() {
+    echo_stderr "DEBUG *** $@"
 }
 
 
 pulserestoreconf() {
-    if [ "${paconfcreated}" == "true" ]; then
-	# conf file was created by this script; remove it
-	rm "${paconf}"
+    ## restores pulseaudio client configuration to state before running
+    ## this script
+
+    if [ "${PA_CONF_CREATED}" == "true" ]; then
+	## conf file was created by this script; remove it
+	rm "${PA_CLIENTCONF_FILE}"
     else
-	if [ "${paconfrestore}" == "true" ]; then
-	    # restore existing conf file
-	    mv "${paconfbackup}" "${paconf}"
+	if [ "${PA_CONF_RESTORE}" == "true" ]; then
+	    ## restore existing conf file
+	    mv "${PA_CLIENT_CONF_BACKUP}" "${PA_CLIENTCONF_FILE}"
 	fi
     fi
 }
 
 pulsebackupconf() {
-    # remove existing script created backup file 
-    if [ -f "${paconfbackup}" ] ; then
-	rm "${paconfbackup}"
-    fi
-    # backup conf
-    cp "${paconf}" "${paconfbackup}"
+    ## remove existing backup file of pulseaudio client configuration
+    ## created by script
+
+    ## clean up from previous attempts
+    [[ -f "${PA_CLIENT_CONF_BACKUP}" ]] && rm "${PA_CLIENT_CONF_BACKUP}"
+
+    cp "${PA_CLIENTCONF_FILE}" "${PA_CLIENT_CONF_BACKUP}"
+
 }
 
-pulsekill() {
-
-        # kill running pulseaudio for current user
-	pulseaudio --kill
-	
-        # check if this succeeded
-	pakillok="$(pgrep -u ${USER} pulseaudio)"
-}
 
 pulsedisable() {
+    ## detects if pulseaudio is running and if os, temporary disables
+    ## and kills it
 
-    checkparunning
+    ## check if pulseaudio is running for the current user
+    parunning="$(pgrep -u ${USER} pulseaudio)"
 
     if [ ! "$?" -eq "1" ]; then
-	echo "pulseaudio is running; will temporary disable and stop it "
+	inform "pulseaudio is running; will temporary disable and stop it ..."
     	
         # temporary disable pulseaudio
-	if [ -f "${paconf}" ] ; then
+	if [ -f "${PA_CLIENTCONF_FILE}" ] ; then
 	    pulsebackupconf
 	    if [ "$?" -eq "0" ]; then
-		sed -i 's/autospawn[[:space:]]*=[[:space:]]*\([no]*\)/autospawn = no/gI' ${paconf} 
+		sed -i 's/autospawn[[:space:]]*=[[:space:]]*\([no]*\)/autospawn = no/gI' ${PA_CLIENTCONF_FILE} 
 		paconfrestore="true"
 	    fi
 	else
-	    echo "autospawn = no" > "${paconf}"
+	    echo "autospawn = no" > "${PA_CLIENTCONF_FILE}"
 	    paconfcreated="true"
 	fi
 	
-	pulsekill
+        ## kill pulseaudio
+	pulseaudio --kill
 
-	if [ -! "$?" -eq "1" ] ; then
-	    # pulse is still running
+	## check if that worked
+	pakillok="$(pgrep -u ${USER} pulseaudio)"
+
+	if [ "$?" = "0" ] ; then
+	    ## pulse is still running
 	    pulserestoreconf
-	    echo "Error: could not kill pulseaudio, exiting script ..."
-	    exit 1
+	    die "Could not kill pulseaudio."
 	fi
     fi
 }
 
-alsacards() {
+fetch_alsadevices_list() {
+    ## displays/returns a formatted list of alsa cards, each with its
+    ## digital output(s)
 
-    boutput=""
+    MSG_NODIGITAL_OUTPUTS="No alsa cards with digital outputs found."
+
+    cmd_output=""
     
-    # construct list of alsa digital outputs
-    aplayoutput="$(aplay -l | grep ^card | grep -i -E 'usb|digital|hdmi|i2s|spdif|toslink|adat')"
+    ## construct list of alsa digital outputs
+    aplay_output="$(aplay -l | grep ^card | grep -i -E 'usb|digital|hdmi|i2s|spdif|toslink|adat')"
 
+    [[ -z "${aplay_output}" ]] && die "${MSG_NODIGITAL_OUTPUTS}"
+
+    ## loop through each line of aplay output   
     while read -r line; do
 
-	# bashism
 	if [[ "${line}" =~ "card "([0-9]*)": "(.*)" ["(.*)"], device "([0-9]*)": "(.*)" ["(.*)"]"(.*) ]]; then
-	    let alsaoutputdevices+=1
+	    let ALSA_OUTPUT_DEVICES+=1
 	    cardnr="${BASH_REMATCH[1]}"
 	    cardname="${BASH_REMATCH[2]}"
 	    cardlabel="${BASH_REMATCH[3]}"
 	    devnr="${BASH_REMATCH[4]}"
 	    devname="${BASH_REMATCH[5]}"
 	    devlabel="${BASH_REMATCH[6]}"
-	    address="hw:${cardnr},${devnr}"
+	    hw_address="hw:${cardnr},${devnr}"
 
-	    boutput=$(echo -e "${boutput}\n${alsaoutputdevices}) ${address} (Soundcard \"${cardlabel}\" with output device \"${devlabel}\")")
+	    ## add the hardware address to the array
+	    ALSA_HW_ADDRESSES=("${ALSA_HW_ADDRESSES[@]}" "${hw_address}")
+ 
+	    cmd_output=$(echo -e "${cmd_output} ${ALSA_OUTPUT_DEVICES}) ${hw_address} (Card \"${cardlabel}\" with output device \"${devlabel}\")")
+	    cmd_output="${cmd_output}\n"
 
-	    if [ ${alsaoutputdevices} -eq "1" ] ; then
-		defaultcard="${address}"
-	    fi
 	fi
 
-    done <<< "${aplayoutput}"
+    done <<< "${aplay_output}"
 
-    echo -e "${boutput}"
+    [[ ${#ALSA_HW_ADDRESSES[@]} -gt 1 ]] && DEVS="s"
+    inform "\nDetected alsa device${DEVS} with digital output${DEVS}:"
+
+    ## display/return the cards found
+    echo -e "${cmd_output}"
+    
+    select_hw_address
+
 }
 
-alsadetect() {
+pop_hw_address() {
+    ## pop the hw_address specified by $1 from the array
+    ## rerun hw_address as long as the array is not empty
 
-    # list all alsa playback devices with their hw:x,y address
-    echo -e "\nDetected alsa devices with digital outputs:"
-
-    # fetch card list
-    alsacards
-
-    if [ ! "${alsaoutputdevices}" -eq "1" ]; then
-	## multiple outputdevices detected
-        ## ask the user to choose one of them
-	echo -e "\nEnter the hardware address of the alsa output device to test"
-	alsadevicelabel="$(read -e -p "(press ENTER to select the default): " \
- -i "${defaultcard}" alsadev && echo $alsadev)"
-    else
-	alsadevicelabel="${defaultcard}"
+    hw_address="$1"
+    declare -a ALSA_HW_ADDRESSES=( ${ALSA_HW_ADDRESSES[@]/${hw_address}/} )
+    if [ ${#ALSA_HW_ADDRESSES[@]} -gt 0 ]; then
+	select_hw_address
     fi
+}
 
+select_hw_address() {
+    ## select a hardware address from the array. When there's mpre
+    ## than one device, prompts the user which one to examine,
+    ## otherwise select it by default
+
+    MSG_SELECT_ALSADEV="\nEnter the hardware address of the alsa output device to test"
+    MSG_ENTER_DEFAULT="and press [ENTER] to select the default: "
+
+    if [ "${#ALSA_HW_ADDRESSES[@]}" = "1" ]; then
+	SELECTED_HW_ADDRESS="${ALSA_HW_ADDRESSES[0]}"
+	inform "\n"
+	read -p "Press [ENTER] to probe device \`${SELECTED_HW_ADDRESS}' ..."
+	list_alsa_output_formats
+	detect_stream
+    else
+	inform "${MSG_SELECT_ALSADEV}"
+	SELECTED_HW_ADDRESS="$(read -e -p "${MSG_ENTER_DEFAULT}" -i "${ALSA_HW_ADDRESSES[0]}" alsadev && echo ${alsadev})"
+	list_alsa_output_formats
+	detect_stream
+	pop_hw_address "${SELECTED_HW_ADDRESS}"
+    fi
+    
+}
+
+
+list_alsa_output_formats() {
+    ## list digital output devices for selected alsa card with their
+    ## hw:x,y address
+    
     ## test whether the device is not in use by other programs
     alsabusy="$(cat /dev/urandom | \
-LANG=C aplay -D ${alsadevicelabel} 2>&1 >/dev/null | \
+LANG=C aplay -D ${SELECTED_HW_ADDRESS} 2>&1 >/dev/null | \
 grep busy)"
     
     if [ ! "$?" -eq "0" ]; then
@@ -142,91 +205,93 @@ grep busy)"
     ## `--dump-hw-params' in alsa-utils 1.0.27, Debian Wheezy = 1.0.25
 	
 	alsaformats="$(cat /dev/urandom | \
-LANG=C aplay -D ${alsadevicelabel} 2>&1 >/dev/null | \
+LANG=C aplay -D ${SELECTED_HW_ADDRESS} 2>&1 >/dev/null | \
 grep '^-' | sed 's/^- //g' )"
 	    
-	echo -e "\nThe selected alsa output device ${alsadevicelabel} \
-supports the following \ndigital audio formats:"
+	inform "\n\`${SELECTED_HW_ADDRESS}' supports the following digital audio formats:"
 
 	index=0
 	while read -r line; do
 	    let index+=1
-	    echo -e "${index}) ${line}"
+	    echo -e " ${index}) ${line}"
 	done <<< "${alsaformats}"
     else
-	echo -e "\nError: Could not determine the supported digital \
-formats for ${alsadevicelabel} \n       because it is in use ..."
+	## construct the path to the device node (ie `/dev/snd/pcmCxDyp`)
+	dev_snd_device="$(echo -e "${SELECTED_HW_ADDRESS}" | sed "s#hw:\([0-9]*\),\([0-9]*\)#/dev/snd/pcmC\1D\2p#")"
+	lsof_out="$(lsof -F c ${dev_snd_device})"
+	p_name="$(echo -e "${lsof_out}" | grep ^c | sed 's/^c\(.*\)$/\1/')"
+	p_id="$(echo -e "${lsof_out}" | grep ^p | sed 's/^p\(.*\)$/\1/')"
+	if [ "${p_name}" = "mpd" ]; then
+	    inform "NOTICE: Could not determine the supported digital formats for \
+\`${SELECTED_HW_ADDRESS}' \nbecause it is in use by mpd. You might want to pause mpd\
+\nusing your favourite mpd client and rerun this script."
+	else
+	    inform "NOTICE: Could not determine the supported digital formats for \
+\n\`${SELECTED_HW_ADDRESS}' because it is in use by \`${p_name}' with process id \`${p_id}'."
+	fi
 	return 1
     fi
 
 }
 
-function pause() {
-   read -p "$*"
-}
 
-watchstream() {
-
-    detectstream
-
-    if [ "$?" -eq "0" ]; then
-	pause "Press [ENTER] when you want to monitor changes to the stream file" 
-	watch -n 0.1 "cat ${procusbstream}"
-    else
-	echo -e "Error: could not find any stream file \
-in ${procasound}.\n"
-	echo -e "Do you actually use a USB Audio Class \
-DA-converter?"
-	
-    fi
-}
-
-detectstreamalt() {
-    echo -e "Warning: stream file not found at expected \
- place:\n ${procusbstream}\nwill try finding it elsewhere \
- within ${procasound} ..."
-    wildguess="$(find ${procasound} -name '*stream*')"
-    if [ ! -z "${wildguess}" ]; then
-	echo -e "Could not determine which stream file to \
-watch, found the following:\n"
-	echo -e "${wildguess}"
-    else
-	return 1
-    fi
-
-}
-
-detectstream() {
-
+detect_stream() {
     ## map the selected sound card to the appropriate 
-    ## USB Audio Class stream in the file 
+    ## USB audio class stream file in
     ## /proc/asound/cardX/streamY
 
+    MSG_NO_STREAM="Unable to monitor changed modes of device"
+    MSG_NO_UACMODULE="\nModule \`snd_usb_audio' not loaded; therefore no USB audio class \
+\ndevices are present.${MSG_NO_STREAM}"
+    MSG_NO_UACCARD="\n${MSG_NO_STREAM} \`${SELECTED_HW_ADDRESS}', because it is \
+\nnot a USB audio class device."
+
+    Q_WATCH_STREAM="\nPress [ENTER] when you want to monitor the stream "
+    Q_WATCH_INSTRUCT="\nreact when playing different digital audio formats using your mpd client.\n "
+    Q_BREAK_WATCH="Press [CTRL+C] to quit or exit the \`watch' screen ."
+
+    ## detect if module snd_usb_audio is loaded
     if [ "$(lsmod | grep snd_usb_audio)" ]; then
-	## snd_usb_audio loaded
-	defusbstream="$(echo -e "hw:1,0" | \
-sed "s#hw:\([0-9]*\),\([0-9]*\)#${procasound}/card\1/stream\2#")"
+	defusbstream="$(echo -e "${SELECTED_HW_ADDRESS}" | \
+sed "s#hw:\([0-9]*\),\([0-9]*\)#${PROC_ASOUND}/card\1/stream\2#")"
+
 	if [ -f "${defusbstream}" ]; then
-	    procusbstream="${procusbstream}"
+	    inform "${Q_WATCH_STREAM}\`${defusbstream}'${Q_WATCH_INSTRUCT}"
+	    read -p "${Q_BREAK_WATCH}" 
+	    watch -n 0.1 cat ${defusbstream}
 	else
-	    detectstreamalt
+	    ## UAC module loaded but selected card is not a UAC device
+	    inform "${MSG_NO_UACCARD}"
+	    #select_hw_address
 	fi
     else
-	## snd_usb_audio not loaded
-	echo -e "Error: module snd_usb_audio not loaded"
-	return 1
+	inform "${MSG_NO_UACMODULE}"
     fi
 
 }
 
-if [[ -f ${paconf} ]]; then
+
+## main
+
+
+if [[ -f ${PA_CLIENTCONF_FILE} ]]; then
 
     pulsedisable
-    alsadetect
+    #list_alsa_output_formats
+    ## fetch list with alsa cards and outputs
+    fetch_alsadevices_list
+
+    ## if multiple output devices were detected list them and
+    ## ask the user to choose the one to use
+    # select_hw_address
+
     pulserestoreconf
 else
-    alsadetect
+    #list_alsa_output_formats
+    fetch_alsadevices_list
+
+    ## if multiple output devices were detected list them and
+    ## ask the user to choose the one to use
 fi
 
-watchstream
-
+#detect_stream
